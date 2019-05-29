d3583d66206c6396be38bb5203b0ce96c3128f46
Rita Bradley <contact@ritabradley.com>
2019-05-29 13:36:14 -0700

    play with arrays

---
+++ arrays.php
@@ -0,0 +1,27 @@
+<?php
+#Array - variable that hold multiple values
+
+/*
+    - Indexed
+    - Associative
+    - Multi-dimensional
+*/
+
+
+// Indexed
+
+$people = ['Rita', 'Ron', 'Isaiah'];
+$ids = [23, 64, 43];
+
+// echo "<h2>$people[1]</h2>";
+// echo "<h2>$ids[0]</h2>";
+
+// print_r($people);
+// var_dump($ids);
+
+
+// Associative
+$people = ["Rita" => 32, "Ron" => 29, "Isaiah" => 8];
+
+echo $people["Ron"];
+// print_r($people);
