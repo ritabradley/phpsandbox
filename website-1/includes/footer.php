a5a0ef42bb69f3e74c2947f2ad0ad57ef39b2dab
Rita Bradley <contact@ritabradley.com>
2019-05-29 18:07:05 -0700

    includes

---
+++ website-1/includes/footer.php
@@ -0,0 +1,6 @@
+<footer>
+  <p>&copy; Rita Bradley 2019</p>
+</footer>
+</body>
+
+</html>
---
+++ website-1/includes/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Generic Website</title>
+</head>
+
+<body>
+
+  <nav>
+    <ul>
+      <li>
+        <a href="index.php" target="_blank" rel="noopener noreferrer">Home</a>
+      </li>
+      <li>
+        <a href="about.php" target="_blank" rel="noopener noreferrer">About</a>
+      </li>
+      <li>
+        <a href="contact.php" target="_blank" rel="noopener noreferrer">Contact</a>
+      </li>
+    </ul>
+  </nav>
